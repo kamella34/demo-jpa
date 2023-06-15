d0475f23a63d90c0749247476f1816e3074a900b
kamella34 <vergnaud.kamella@bbox.fr>
2023-06-15 14:52:08 +0200

    Tp2

---
+++ src/main/java/fr/digi/m0323/entité/Livre.java
@@ -0,0 +1,68 @@
+package fr.digi.m0323.entité;
+
+import jakarta.persistence.*;
+
+
+//@Entity(name="livre")
+
+@Entity
+@Table(name="livre")
+
+public class Livre {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+@Column(name="titre")
+private String titre;
+  @Column(name="auteur")
+private String auteur;
+
+  public Livre() {
+
+  }
+
+  public Livre(String titre, String auteur) {
+    this.titre = titre;
+    this.auteur = auteur;
+  }
+
+  public Livre(Integer id, String titre, String auteur) {
+    this.id = id;
+    this.titre = titre;
+    this.auteur = auteur;
+  }
+
+  @Override
+  public String toString() {
+    return "Livre{" +
+            "id=" + id +
+            ", titre='" + titre + '\'' +
+            ", auteur='" + auteur + '\'' +
+            '}';
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitre() {
+    return titre;
+  }
+
+  public void setTitre(String titre) {
+    this.titre = titre;
+  }
+
+  public String getAuteur() {
+    return auteur;
+  }
+
+  public void setAuteur(String auteur) {
+    this.auteur = auteur;
+  }
+}
